b0f8609347f56b4b8f175a490f2fe3eca316376a
Conor Anderson <conor@conr.ca>
2017-01-23 19:30:27 -0500

    Add monthly `quick_audit()`

--- R/quick_audit.R
+++ R/quick_audit.R
@@ -4,6 +4,7 @@
 ##'
 ##' @param station character; a station id number to process.
 ##' @param variables numeric or character; By default, all variables will be included. Pass a numeric vector to specify columns to include, or pass a character vector to try to match column names. 
+##' @param by character; whether values should be reported annually (\code{by = year}), or monthly (\code{by = month}). 
 ##' @param reverse Boolean; if true, will show proportion present instead of proportion missing.
 ##' 
 ##' @importFrom tibble tibble add_column
@@ -14,7 +15,7 @@
 ##'
 ##' @examples
 ##' \dontrun{quick_audit("000401", "Tmax", reverse = TRUE)}
-##' \dontrun{quick_audit("000401", 2:10)}
+##' \dontrun{quick_audit("000401", 2:10, by = "month")}
 
 quick_audit <- function(station, variables, reverse = FALSE) {
   
@@ -37,23 +38,52 @@ quick_audit <- function(station, variables, reverse = FALSE) {
   else {
     ctl = 0
   }
-
+  
   years <- min(format(dat$Fecha, format = "%Y")):max(format(dat$Fecha, format = "%Y"))
   
-  out <- tibble(Year = years)
-  
-  for (var in variables) {
-    integrity <- NULL
-    for (year in years) {
-      colname <- colnames(dat)[var]
-      yearly <- dat[format(dat$Fecha, format = "%Y") == year,]
-      obs <- nrow(yearly)
-      missing <- sum(is.na(yearly[[var]]))
-      integrity <- c(integrity, abs(ctl-(missing/obs)))
+  if (by == "month") {
+    months <- min(format(dat$Fecha, format = "%m")):max(format(dat$Fecha, format = "%m"))
+    yearmons <- apply(expand.grid(sprintf("%02d", months), years), 1, function(x) paste(x[2], x[1], sep = "-"))
+    
+    out <- tibble(`Year-month` = yearmons)
+    
+    for (var in variables) {
+      integrity <- NULL
+      for (yearmon in yearmons) {
+        colname <- colnames(dat)[var]
+        yearmonly <- dat[format(dat$Fecha, format = "%Y-%m") == yearmon,]
+        obs <- nrow(yearmonly)
+        missing <- sum(is.na(yearmonly[[var]]))
+        integrity <- c(integrity, abs(ctl-(missing/obs)))
+      }
+      out <- add_column(out, newcol = integrity)
+      names(out)[which(names(out) == "newcol")] <- colname
     }
-    out <- add_column(out, newcol = integrity)
-    names(out)[which(names(out) == "newcol")] <- colname
+    
+    out$`Year-month` <- as.yearmon(out$`Year-month`)
+    out <- add_column(out, Year = format(out$`Year-month`, format = "%Y"), Month = format(out$`Year-month`, format = "%m"), .before = 1)
+    
+  } else {
+    
+    if (by == "year") {
+      out <- tibble(Year = years)
+      
+      for (var in variables) {
+        integrity <- NULL
+        for (year in years) {
+          colname <- colnames(dat)[var]
+          yearly <- dat[format(dat$Fecha, format = "%Y") == year,]
+          obs <- nrow(yearly)
+          missing <- sum(is.na(yearly[[var]]))
+          integrity <- c(integrity, abs(ctl-(missing/obs)))
+        }
+        out <- add_column(out, newcol = integrity)
+        names(out)[which(names(out) == "newcol")] <- colname
+      }
+      
+    } else {
+      stop("You must choose by \"month\", or by \"year\".")
+    }
   }
   return(out)
 }
-  
