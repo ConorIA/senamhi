6c7211b86da0f39e129ba3a746ff69d1b24fcc2b
Conor Anderson <conor@conr.ca>
2018-09-27 22:11:12 +0000

    Update install_senamhiR.R [skip ci]

--- install_senamhiR.R
+++ install_senamhiR.R
@@ -1,21 +1,13 @@
 cat("##### PSA ##### \n 
-You are about to run remote code! It is always a good idea to review remote code \
-before running, as a malicious actor could use a script like this to do naughty \
-things on your computer.\n \
-In this case, we are going to install the senamhiR package, and a couple of \
-packages that we need to install it.\n\n")
+You are about to run remote code! It is always a good idea to review remote \
+code before running, as a malicious actor could use a script like this to do \
+naughty things on your computer.\n \
+In this case, we are going to install the remotes package and use it to install\
+senamhiR.\n\n")
 
-proceed <- readline(prompt = "Would you like to proceed? (y/N)")
-
-if (proceed == "Y" || proceed == "y") {
-  if (!any(c("devtools", "remotes") %in% rownames(installed.packages()))) {
-    install.packages("devtools")
-  }
-  if("devtools" %in% rownames(installed.packages()) == TRUE) {
-    devtools::install_gitlab("ConorIA/senamhiR")
-  } else {
-    remotes::install_gitlab("ConorIA/senamhiR")
-  }
+if (readline("Would you like to proceed? (y/N)") %in% c("Y", "y")) {
+  source("https://install-github.me/r-lib/remotes")
+  remotes::install_gitlab("ConorIA/senamhiR")
 } else {
   stop("Review the remote code and try again.")
 }
